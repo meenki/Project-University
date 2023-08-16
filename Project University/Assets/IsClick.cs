c4978d4229cc16a0e0201f412047084bd76d5d70
dp2421 <80231209+dp2421@users.noreply.github.com>
2023-08-16 16:44:52 +0900

    로비 신 수정 및 설정창 프리펩 생성

---
+++ Project University/Assets/IsClick.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IsClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject TargetObj;
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(ButtonClickReaction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ButtonClickReaction()
+    {
+        if (TargetObj != null)
+        {
+            if (TargetObj.activeSelf)
+            {
+                TargetObj.SetActive(false);
+            }
+            else
+            {
+                TargetObj.SetActive(true);
+            }
+            // 해당하는 버튼에 맞춰 끄기
+        }
+    }
+}
